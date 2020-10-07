f3443ff0932f9b64e3606e6cdd90c5396ee4133b
Anh Nguyen <andy.a.d.nguyen@zoho.com>
2020-10-07 14:45:11 -0700

    Implemented functional stack.js

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  storage.size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage.size++;
+    storage[storage.size - 1] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[storage.size - 1];
+    delete storage[storage.size - 1];
+    storage.size--;
+    return popped;
   };
 
   someInstance.size = function() {
+    if (storage.size < 0) {
+      storage.size = 0;
+    }
+    return storage.size;
   };
 
   return someInstance;
