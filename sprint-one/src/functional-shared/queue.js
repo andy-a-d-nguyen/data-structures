23e11fd6ac9f54f926417d013e76e0f3be361138
Anh Nguyen <andy.a.d.nguyen@zoho.com>
2020-10-07 21:09:23 -0700

    Implemented functional-shared stack.js

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create an instance of Queue class
+  var someInstance = {};
+
+  // create a .length property of the instance and set it to 0
+  someInstance.length = 0;
+
+  // using _.extend, copy the properties of the source object to the destination object
+  _.extend(someInstance, queueMethods);
+
+  // return the instance of Queue class
+  return someInstance;
 };
 
+// the object from which someInstance of Queue will get its properties (source)
 var queueMethods = {};
+
+
 
 
