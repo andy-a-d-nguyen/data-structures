9a56067d203ccc6d16e03a9bf53e588ea2aaa3b7
Anh Nguyen <andy.a.d.nguyen@zoho.com>
2020-10-08 10:34:29 -0700

    Implemented es6 queue.js

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -8,4 +8,38 @@ class Queue {
   }
 
   // .size method of the class Stack's prototype
+  size() {
+
+    if (this.length < 0) {
+      this.length = 0;
+    }
+
+    return this.length;
+  }
+
+  // .push method of the class Stack's prototype
+  enqueue(value) {
+
+    this.length++;
+
+    this[this.length - 1] = value;
+
+    return this.length;
+  }
+
+  // .shift method of the class Stack's prototype
+  dequeue() {
+
+    var dequeued = this[0];
+
+    for (var index in this) {
+      this[index - 1] = this[index];
+    }
+
+    delete this[this.length - 1];
+
+    this.length--;
+
+    return dequeued;
+  }
 }
