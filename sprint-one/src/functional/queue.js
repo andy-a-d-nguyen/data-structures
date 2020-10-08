1e4a4b6b283e35facfb789ab9bc61fdb8e8ab3f8
Anh Nguyen <andy.a.d.nguyen@zoho.com>
2020-10-07 17:59:13 -0700

    Implemented functional queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,45 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  storage.size = 0;
   // Implement the methods below
 
+  // add input value to the back of the queue (push)
   someInstance.enqueue = function(value) {
+    // increment size by 1
+    storage.size++;
+    // assign input value to 'last index/key'
+    storage[storage.size - 1] = value;
   };
 
+  // remove and return value at the front of the queue (shift)
   someInstance.dequeue = function() {
+    // create a variable to store removed value
+    // assign variable to value at 'first index/key'
+    var dequeued = storage[0];
+    // delete value at 'first index/key'
+    delete storage[0];
+    // iterate over storage
+    for (var key in storage) {
+      // assign value at current key to key that is 1 less than current key
+      storage[key - 1] = storage[key];
+    }
+    // delete the last property because its value now lies in new index that is 1 less and is not needed
+    delete storage[storage.size - 1];
+    // decrement size by 1
+    storage.size--;
+    // return removed value
+    return dequeued;
   };
 
   someInstance.size = function() {
+    // if size is less than 0
+    if (storage.size < 0) {
+      // assign size to be 0
+      storage.size = 0;
+    }
+    // return size
+    return storage.size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,25 +3,35 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // create a .size property to mirror array's .length property
   storage.size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // increment size by 1 when a new value is added
     storage.size++;
+    // assign value to a key that is 1 less than the size
     storage[storage.size - 1] = value;
   };
 
   someInstance.pop = function() {
+    // create a variable to store the popped value
     var popped = storage[storage.size - 1];
+    // delete the property at the 'last index/key'
     delete storage[storage.size - 1];
+    // decrement size by 1 when a property is removed
     storage.size--;
+    // return the popped value
     return popped;
   };
 
   someInstance.size = function() {
+    // if size is less than 0
     if (storage.size < 0) {
+      // set size to 0
       storage.size = 0;
     }
+    // return size property
     return storage.size;
   };
 
