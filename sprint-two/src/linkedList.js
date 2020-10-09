8b2b36726f8d6df6643419525173de2e3b290448
Blake-Whitham <bwhitham77@gmail.com>
2020-10-08 17:23:00 -0700

    Implemented LinkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,18 +1,39 @@
 var LinkedList = function() {
   var list = {};
+
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    this.tail = Node(value);
+    if (list.head === null) {
+      this.head = Node(value);
+    }
+    list.head.next = list.tail;
   };
 
   list.removeHead = function() {
+    var save = this.head.value;
+    this.head = this.head.next;
+    delete save;
+
+    return save;
   };
 
   list.contains = function(target) {
+    var current = this.head;
+    while (current.next !== null) {
+      if (current.value === target || current.next.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
+
   return list;
+
 };
 
 var Node = function(value) {
@@ -24,6 +45,8 @@ var Node = function(value) {
   return node;
 };
 
+
+
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? // Linear
  */
