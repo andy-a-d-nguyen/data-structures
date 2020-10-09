4a0e8a06b3e9ba73190d4be1df520c6e69a7e8da
Blake-Whitham <bwhitham77@gmail.com>
2020-10-09 15:35:03 -0700

    Implemented Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,86 @@
 
 
 // Instantiate a new graph
-var Graph = function() {
+var Graph = function () {
+  // create a data structure to store nodes
+  this.nodes = {};
+  // create a data structure to store edges
+  this.edges = {};
 };
+// var Node = function(value) {
+//   this.value = value;
+//   this.edges =
+// }
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function (node) {
+  // add input node to the list of adjacent nodes
+  this.nodes[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function (node) {
+  // if node is in graph
+  // return true
+  // return false
+  if (this.nodes[node]) {
+    return true;
+  }
+  return false;
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function (node) {
+  // if graph contains node
+  // remove node
+  if (this.contains(node)) {
+    delete this.nodes[node];
+    delete this.edges[node];
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-};
+Graph.prototype.hasEdge = function (fromNode, toNode) {
+  // if fromNode and toNode are in list of adjacent nodes
+  // return true
+  // return false
 
+  if (fromNode in this.edges && toNode in this.edges) {
+    return true;
+  }
+  return false;
+};
+// nodes = {
+// {2: 2, 3: 3},
+// {1: 1},
+// {3: 3, 2: 2}
+// }
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
+// 3      2
+Graph.prototype.addEdge = function (fromNode, toNode) {
+  // add fromNode and toNode to list of adjacent nodes
+  this.edges[fromNode] = fromNode;
+  this.edges[toNode] = toNode;
+
+  // this.nodes[fromNode].toNode = toNode;
+  // this.nodes[toNode].fromNode = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
+Graph.prototype.removeEdge = function (fromNode, toNode) {
+  delete this.edges[fromNode];
+  delete this.edges[toNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
+Graph.prototype.forEachNode = function (cb) {
+  for (var node in this.nodes) {
+    cb(this.nodes[node]);
+  }
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? 01 for all but forEachNode(linear / 0n)
  */
 
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,7 +4,7 @@ var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
-
+//k = key, v = value;
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
@@ -23,4 +23,23 @@ HashTable.prototype.remove = function(k) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+//maintains associations between two data values as key and value,
+//a single instance of a hash table may store many associations
 
+// synonyms:
+// associative array, Dictionary, map, or Hash
+
+
+//====RULES====//
+//-references values (data) at an index within the array bounds (0 to length-1), stores that in an array
+//-always output the same index value
+//-doesnt remember previously used array locations or input strings
+
+//- an index point has an embeded array that holds all prior associations ->
+// ''''at there assigned random index value'''' or at a key value pair associative object
+
+// ---------- //
+
+// tuple = fixed length array, with fixed indexed values
+
+//Structure storage array to be filled 50-75% by the data
